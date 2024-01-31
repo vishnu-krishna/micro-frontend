8be3b08f06addd2587916774834169ee4ee4353a
Vishnu <vbalakrishnan@seek.com.au>
2024-01-31 16:38:48 +1100

    add css

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
 import MarketingApp from '../components/MarketingApp';
+import Header from '../components/Header';
+import { createGenerateClassName, StylesProvider } from '@material-ui/core/styles';
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co',
+});
 
 export default () => {
     return (
-        <div>
-            <h1>Hi there!</h1>
-            <hr/>
-            <MarketingApp/>
-        </div>
+        <BrowserRouter>
+            <StylesProvider generateClassName={generateClassName}>
+                <div>
+                    <Header/>
+                    <MarketingApp/>
+                </div>
+            </StylesProvider>
+        </BrowserRouter>
     );
 };
