78eb6afde2105a80fddb8b96b2c281d9ce1c998d
Vishnu <vbalakrishnan@seek.com.au>
2024-01-31 18:04:58 +1100

    add unique css for marketing

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles';
-
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
+import { createGenerateClassName, StylesProvider } from '@material-ui/core/styles';
 
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'marketing',
+});
 export default () => {
     return (
         <div>
-            <StylesProvider>
+            <StylesProvider generateClassName={generateClassName}>
                 <BrowserRouter>
                     <Switch>
                         <Route exact path='/pricing' component={Pricing}/>
