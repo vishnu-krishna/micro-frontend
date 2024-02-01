df6171291a3e7a33695588fbd63528ef898624d8
Vishnu <vbalakrishnan@seek.com.au>
2024-02-01 19:20:23 +1100

    use browser history in container and memory history in sub app

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Landing from './components/Landing';
-import Pricing from './components/Pricing';
+import { Route, Router, Switch } from 'react-router-dom';
 import { createGenerateClassName, StylesProvider } from '@material-ui/core/styles';
 
+import Landing from './components/Landing';
+import Pricing from './components/Pricing';
 
 const generateClassName = createGenerateClassName({
     productionPrefix: 'marketing',
 });
-export default () => {
+
+export default ({ history }) => {
     return (
         <div>
             <StylesProvider generateClassName={generateClassName}>
-                <BrowserRouter>
+                <Router history={history}>
                     <Switch>
                         <Route exact path='/pricing' component={Pricing}/>
                         <Route path='/' component={Landing}/>
                     </Switch>
-                </BrowserRouter>
+                </Router>
             </StylesProvider>
         </div>
     );
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,16 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createBrowserHistory, createMemoryHistory } from 'history';
 import App from './App';
 
-const mount = (el) => {
-    ReactDOM.render(<App/>, el);
+// Mount function to start up the app
+const mount = (el, { onNavigate, defaultHistory }) => {
+    const history = defaultHistory || createMemoryHistory();
+
+    if (onNavigate) {
+        history.listen(onNavigate);
+    }
+
+    ReactDOM.render(<App history={history}/>, el);
+
+    return {
+        onParentNavigate({ pathname: nextPathname }) {
+            const { pathname } = history.location;
+
+            if (pathname !== nextPathname) {
+                history.push(nextPathname);
+            }
+        },
+    };
 };
 
+// If we are in development and in isolation,
+// call mount immediately
 if (process.env.NODE_ENV === 'development') {
     const devRoot = document.querySelector('#_marketing-dev-root');
+
     if (devRoot) {
-        mount(devRoot);
+        mount(devRoot, { defaultHistory: createBrowserHistory() });
     }
 }
 
+// We are running through container
+// and we should export the mount function
 export { mount };
